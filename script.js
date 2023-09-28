1a3baefa91cbc0a90dd23472baf83d3e85c3b5f3
Witzzz <143587485+GorWitzz@users.noreply.github.com>
2023-09-28 15:46:29 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const buyButtons = document.querySelectorAll('button');
-
-  buyButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      alert('Terima kasih telah membeli hp ini!');
-    });
-  });
-});
